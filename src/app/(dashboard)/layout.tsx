35ca928acd5171d7e96bafc22e2e8229d3be3071
SuryaKF <suryakf04@gmail.com>
2024-10-16 20:04:34 +0530

    Minor changes in menu and the layout.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/Menu";
+
 export default function DashbordLayout({
     children,
   }: Readonly<{
@@ -5,8 +9,14 @@ export default function DashbordLayout({
   }>) {
     return <div className="h-screen flex">
       {/* LEFT*/}
-      <div className="w-[14%] md:w[8]lg:w-[16%] bg-red-200">l</div>
+      <div className="w-[14%] md:w-[8]lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src="/logo.png"alt="logo"width={32} height={32}/>
+          <span className="hidden lg:block">Divine School</span>
+        </Link>
+        <Menu/>
+      </div>
       {/* RIGHT*/}
-      <div className="w-[86%] md[92%] bg-blue-200">r</div>
+      <div className="w-[86%] md:w-[92%] lg:w[84%] xl:w-[86%] bg-blue-200">r</div>
     </div>;
   }

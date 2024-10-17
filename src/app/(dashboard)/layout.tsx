8089c5bcddf29e51711802627c80711d613f0b4a
SuryaKF <suryakf04@gmail.com>
2024-10-17 21:11:45 +0530

    Updated the layout height.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,9 +9,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
+    <div className="h-full flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[cyan] p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
@@ -22,7 +22,7 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] h-1 bg-[#F7F8FA] overflow-scroll flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[white] overflow-auto flex flex-col">
         <Navbar />
         {children}
       </div>

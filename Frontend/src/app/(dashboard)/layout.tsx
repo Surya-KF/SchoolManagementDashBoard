020dbf46500569a37c028aad115157d371478793
SuryaKF <suryakf04@gmail.com>
2024-10-22 01:45:04 +0530

    Minor Changes.

--- Frontend/src/app/(dashboard)/layout.tsx
+++ Frontend/src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
   return (
     <div className="h-fit flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-blue-300 p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[blu] p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
@@ -22,7 +22,7 @@ export default function DashboardLayout({
         <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[white] overflow-auto flex flex-col">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         <Navbar />
         {children}
       </div>

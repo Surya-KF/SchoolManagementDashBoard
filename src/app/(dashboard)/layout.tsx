5b51ccef4c16059ebfdc1ec08b5b1d3daa26a27a
SuryaKF <suryakf04@gmail.com>
2024-12-30 20:44:08 +0530

    Updated the src along with the backend integration

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,15 +9,15 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-fit flex">
+    <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-[blu] p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
         >
           <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block font-bold">Divine School</span>
+          <span className="hidden lg:block font-bold">SchooLama</span>
         </Link>
         <Menu />
       </div>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,13 +1,13 @@
 // TEMPORARY DATA
 
-export let role = "admin";
+export let role = "student";
 
 export const teachersData = [
   {
     id: 1,
     teacherId: "1234567890",
-    name: "Bikash Yadav",
-    email: "bikash@yadav.com",
+    name: "John Doe",
+    email: "john@doe.com",
     photo:
       "https://images.pexels.com/photos/2888150/pexels-photo-2888150.jpeg?auto=compress&cs=tinysrgb&w=1200",
     phone: "1234567890",
